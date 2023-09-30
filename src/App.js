b6f2a39ccfc755c109c08388ce54edb43a9dca49
Ahmedelhamzawy <ahmedelhamzawy9@gmail.com>
2023-09-30 16:36:59 +0300

    two e2e tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
-import Stepper from './components/Stepper'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-<Stepper />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 data-cy="app-heading">Vite + React + Cypress</h1>
+      <div className="card">
+        <button data-cy="app-button" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn mooooore
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
