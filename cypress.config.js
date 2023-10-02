4aa7e84aada4e6a04717b3a1e4a7f4613ea29c27
Ahmedelhamzawy <ahmedelhamzawy9@gmail.com>
2023-10-02 19:38:44 +0300

    handling errors

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "4evede",
+
   e2e: {
     baseUrl: "http://localhost:3000/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
